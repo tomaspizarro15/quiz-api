94638dd3fb84183097f7f9161f7a8f94b2a55d87
tomas <tomipizarro@outlook.com>
2021-03-19 14:47:56 -0300

    database connection added

--- bin/api.js
+++ bin/api.js
@@ -1,4 +1,6 @@
 const app = require('../app');
+const mongoose = require('mongoose')
+
 const { graphqlHTTP } = require('express-graphql')
 const rootSchema = require('./../graphQL/root_schema')
 const rootResolver = require('./../graphQL/root_resolver')
@@ -13,4 +15,13 @@ app.get('/', (req, res, next) => {
         status: 200,
     })
 })
-app.listen(process.env.PORT)
+mongoose.connect(process.env.MONGO_URI , {useNewUrlParser : true , useUnifiedTopology : true}).then(() => {
+}).catch(err => {
+    app.use((req ,res) => {
+        res.status(500).json({
+            message : "Not available to connect to Mongodb cluster",
+            status : 500,
+            err : err,
+        })
+    })
+})
--- db/database.js
+++
@@ -1,5 +0,0 @@
-const {Sequelize} = require('sequelize');
-
-const sequelize = new Sequelize(process.env.MYSQL_URI , process.env.MYSQL_USER , process.env.MYSQL_PASSWORD , {dialect : 'mysql' , host: 'localhost'});
-
-module.exports = sequelize;
